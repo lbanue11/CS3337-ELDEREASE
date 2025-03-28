7ac81befa5cfef103c8c9aa43c685182372bb43c
Ramirez <dramir178@calstatela.edu>
2025-03-28 10:07:39 -0700

    Added UI to the Login/Signup/Registration pg

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react';
-import './App.css';
-import Login from './Login';
-import Registration from './Registration'; 
+import { useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import Registration from "./Registration";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Link } from "react-router-dom"; // we using routers to switch pages
+import LogoSymbol from "./assets/LogoSymbol.png";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,12 +13,17 @@ function App() {
     <Router>
       <Routes>
         {/* Default Page for now */}
-        <Route path="/" element={
-          <div>
-            <h1>Welcome to ElderEase</h1>
-            <Link to="/login">Go to Login</Link><br />
-          </div>
-        } />
+        <Route 
+          path="/"
+          element={
+            <div>
+              <img className="logo" src={LogoSymbol} alr="Logo" />
+              <h1 className="app-text-style">Welcome to ElderEase</h1>
+              <Link to="/login">Go to Login</Link>
+              <br />
+            </div>
+          }
+        />
 
         {/* Login router */}
         <Route path="/login" element={<Login />} />
--- front/src/Registration.jsx
+++ front/src/Registration.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import email_icon from "./assets/email_icon.png";
+import password_icon from "./assets/password_icon.png";
+import person_icon from "./assets/person_icon.png";
+
 
 const Registration = () => {
   const [name, setName] = useState('');
@@ -28,30 +32,36 @@ const Registration = () => {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
+    <div className='box-container'>
+      <h1 className='title-text-style'>Register</h1>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name:</label>
+        <div className='input'>
+          <img src={person_icon} />
+          <label> Name</label>
           <input 
             type="text" 
+            className='textbox'
             value={name} 
             onChange={(e) => setName(e.target.value)} 
             required
           />
         </div>
-        <div>
-          <label>Email:</label>
+        <div className='input'>
+          <img src={email_icon} />
+          <label> Email</label>
           <input 
+            className='textbox'
             type="email" 
             value={email} 
             onChange={(e) => setEmail(e.target.value)} 
             required
           />
         </div>
-        <div>
-          <label>Password:</label>
+        <div className='input'>
+          <img src={password_icon} />
+          <label> Password</label>
           <input 
+            className='textbox'
             type="password" 
             value={password} 
             onChange={(e) => setPassword(e.target.value)} 
